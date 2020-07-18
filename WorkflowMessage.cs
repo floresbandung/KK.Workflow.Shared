c7e352a2c4bccce6b6276111f372c5e2bce26afa
kunkun kurniawan <kurnia.wirawan@gmail.com>
2020-07-18 20:26:39 +0700

    add shared workflow message

---
+++ WorkflowMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Workflow.Shared
+{
+    public static class WorkflowMessage
+    {
+        public const string INVALID_CONFIG_NUMBER = "Configuration number not setup yet";
+        public const string INVALID_PROCESS_ACTIVITY = "No template process activity";
+        public const string INVALID_MODULE = "No module available";
+    }
+}
